33de9a19478e069afddec1d0c83f077694edb62d
Manuel Portmann <manuel.portmann@stud.hslu.ch>
2024-09-19 21:35:25 +0900

    change with largest

---
+++ 01_BiggestNumber.py
@@ -0,0 +1,11 @@
+print ("Which number is bigger?")
+
+#input
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+
+#largest integer
+largest = max(a, b, c)
+
+print(f"The largest number is: {largest}")
